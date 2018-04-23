7126646251cea74febb805a0515944e136b899e5
veeraps <vsnchn@gmail.com>
2018-04-23 15:10:40 -0700

    My First CCI

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld
+{
+    public  void method1()
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void test_method1() {
+        HelloWorld hw = new HelloWorld();
+        hw.method1();
+    }
+}
